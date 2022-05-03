e9a8304b2cd5ea8f0c9f1dd7afb76240e6f3b99a
Denis BOURGE <denis.bourge@sharingcloud.com>
2022-05-03 16:27:01 +0000

    fix: rule branch deserialization fix and merge rule SQL

--- crates/github_scbot_types/src/rule_branch.rs
+++ crates/github_scbot_types/src/rule_branch.rs
@@ -2,15 +2,50 @@
 
 use std::{convert::Infallible, str::FromStr};
 
-use serde::{Deserialize, Serialize};
+use serde::{de::Visitor, Deserialize, Serialize};
 
 /// Rule branch.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum RuleBranch {
     /// Named.
     Named(String),
     /// Wildcard.
     Wildcard,
+}
+
+impl Serialize for RuleBranch {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.name())
+    }
+}
+
+struct RuleBranchVisitor;
+
+impl<'de> Visitor<'de> for RuleBranchVisitor {
+    type Value = RuleBranch;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a valid branch name")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(v.into())
+    }
+}
+
+impl<'de> Deserialize<'de> for RuleBranch {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(RuleBranchVisitor)
+    }
 }
 
 impl From<&str> for RuleBranch {
